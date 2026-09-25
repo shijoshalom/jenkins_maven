8b337b307275b335ca7fd0da1ddd64a2b2dda92e
shijoshalom <salamanshijo2002@gmail.com>
2026-09-25 10:03:22 +0530

    Test Jenkins build failure

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,6 +1,7 @@
 package com.example;
 
 public class App {
+	ivalid java code
     public static void main(String[] args) {
         System.out.println("Hello from Jenkins + Maven CI/CD Pipeline!");
     }
